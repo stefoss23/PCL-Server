da50f1ec38c231f5875c06afcc0991fc53ed6c35
Steinar Foss <stefoss23@gmail.com>
2025-12-16 17:20:43 +0100

    server using package pycserv

--- server/cserv_api/views.py
+++ server/cserv_api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from pycserv import *
 
 def number(request):
-    return HttpResponse("tttyyy")
+    return HttpResponse(func())
