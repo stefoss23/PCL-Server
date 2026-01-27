902166f9d120b781c72d778578f395654b1b8088
Steinar Foss <stefoss23@gmail.com>
2026-01-28 00:41:03 +0100

    Can compile python cxx module

--- python/cxx/cserv_pybind.cpp
+++ python/cxx/cserv_pybind.cpp
@@ -1,1 +1,22 @@
-dasfdsf
+#include <math.h>
+#include <iostream>
+#include <exception>
+#include <complex>
+
+#include <Python.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+
+
+
+using namespace std;
+/*using namespace radsim;
+using namespace std::complex_literals;*/
+
+
+
+PYBIND11_MODULE(cserv_python, m) {
+
+  //m.def("complex_equal", &complex_equal);
+
+}
