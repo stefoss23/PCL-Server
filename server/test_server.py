d800beaae83d6812d1f6eb04284fa2944b111642
Steinar Foss <stefoss23@gmail.com>
2026-03-01 18:09:59 +0100

    Started to add code for testing server

--- server/test_server.py
+++ server/test_server.py
@@ -1,10 +1,29 @@
-
+import requests
+import multiprocessing
+import subprocess
+import time
 import unittest
+
+import os
 
 class ServerTester(unittest.TestCase):
 
+    @classmethod
+    def startServer(self):
+        print("-------", os.getcwd())
+        subprocess.run(['timeout', '60s', 'python3', '../../server/manage.py', 'runserver'])
+
+    def startProcess(self):
+        p1 = multiprocessing.Process(target=startServer)
+        p1.start()
+        time.sleep(5)
+
+    @classmethod
+    def setUpClass(cls):
+        cls.startServer()
+
     def test1(self):
-        raise Exception("HHH")
+        pass
 
 
 if __name__ == '__main__':
